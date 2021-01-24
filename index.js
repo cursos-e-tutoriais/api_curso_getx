e14abb1e063c6237b99fc015ef29e8599d231f05
Jefferson Rodrigues <rodriguesjeff.dev@gmail.com>
2021-01-23 23:34:42 -0300

    created initial route

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const router = express.Router();
-
 const app = express();
+app.use('/', router);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+router.get('/api/v1', (req, res) => {
+    res.status(200).json({
+        message: 'Api v1'
+    });
+});
 
 app.listen(8080, (req, res) => {
     console.log('Servidor rodando, Ctrl+c para encerrar');
